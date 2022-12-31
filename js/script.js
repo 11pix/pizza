5f295474ea3585cafa168fa2e5f19bd1acb1d3be
Daniel Fitz <eleven@centrum.cz>
2023-01-01 00:26:18 +0100

    Dokončeno scrollování pro ostatní odkazy

--- js/script.js
+++ js/script.js
@@ -5,5 +5,23 @@
         $(".jq--scroll-our-pizza").click(function(){
             $("html, body").animate({scrollTop: $(".jq--our-pizza").offset().top}, 1000);
         });
+
+        $(".jq--scroll-o-nasi-pizze").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--o-nasi-pizze").offset().top}, 1200);
+        });
+
+        $(".jq--scroll-reference").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--reference").offset().top}, 1400);
+        });
+
+        $(".jq--scroll-fotogalerie").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--fotogalerie").offset().top}, 1600);
+        });
+
+        $(".jq--scroll-kontakt").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--kontakt").offset().top}, 1800);
+        });
+
+
     });
 })(jQuery);
