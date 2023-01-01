74a8ddd2bc3dc5a60a5fcd5943be81888952a8fd
Daniel Fitz <eleven@centrum.cz>
2023-01-01 10:43:39 +0100

    V jQuery upraveno pozicování scrollování přesně nad sekci

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@
 
         /* Scroll to sections */
         $(".jq--scroll-our-pizza").click(function(){
-            $("html, body").animate({scrollTop: $(".jq--our-pizza").offset().top}, 1000);
+            $("html, body").animate({scrollTop: $(".jq--our-pizza").offset().top-70}, 1000);
         });
 
         $(".jq--scroll-o-nasi-pizze").click(function(){
-            $("html, body").animate({scrollTop: $(".jq--o-nasi-pizze").offset().top}, 1200);
+            $("html, body").animate({scrollTop: $(".jq--o-nasi-pizze").offset().top-70}, 1200);
         });
 
         $(".jq--scroll-reference").click(function(){
-            $("html, body").animate({scrollTop: $(".jq--reference").offset().top}, 1400);
+            $("html, body").animate({scrollTop: $(".jq--reference").offset().top-67}, 1400);
         });
 
         $(".jq--scroll-fotogalerie").click(function(){
-            $("html, body").animate({scrollTop: $(".jq--fotogalerie").offset().top}, 1600);
+            $("html, body").animate({scrollTop: $(".jq--fotogalerie").offset().top-87}, 1600);
         });
 
         $(".jq--scroll-kontakt").click(function(){
